88e3965605084eddd0ab4c8f862e26d0955d7810
vasily.tsvetkov <vasily.tsvetkov@multiloginapp.com>
2020-03-15 17:35:32 +0200

    Autogenerate Spring Boot app

---
+++ src/main/java/org/fibo/seo/autocomplete/AutocompleteApp.java
@@ -0,0 +1,13 @@
+package org.fibo.seo.autocomplete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutocompleteApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutocompleteApp.class, args);
+	}
+
+}
---
+++ src/test/java/org/fibo/seo/autocomplete/AutocompleteAppTests.java
@@ -0,0 +1,13 @@
+package org.fibo.seo.autocomplete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutocompleteAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
